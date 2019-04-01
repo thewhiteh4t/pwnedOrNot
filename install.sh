cf53008270686825b3dc79d8af496cbfd3bd070b
thewhiteh4t <lohityapushkar@gmail.com>
2019-04-01 23:41:18 +0530

    Changed to python3

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ apt-get update > install.log
 echo
 echo '[!] Installing Dependencies...'
 echo '    Python'
-apt-get -y install python &>> install.log
+apt-get -y install python3 python3-pip &>> install.log
 echo '    NodeJS'
 apt-get -y install nodejs &>> install.log
 echo '    Requests'
-pip install requests &>> install.log
+pip3 install requests &>> install.log
 echo '    CfScrape'
-pip install cfscrape &>> install.log
+pip3 install cfscrape &>> install.log
 echo
 echo '[!] Installed.'
