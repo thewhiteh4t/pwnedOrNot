d19dd331c3948e752876429fede5deb10982b5eb
thewhiteh4t <lohityapushkar@gmail.com>
2021-08-10 22:15:03 +0530

    install script updated
    Former-commit-id: e64b0c8a02a776556cc184ebc277c0b300bfac68

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
+echo
+echo "> This script will install dependencies and configuration file"
+echo
+echo "> If you encounter any error please fix them before using the tool"
+echo
+echo "[!] Installing Requests..."
+echo "--------------------------"
+pip3 install requests
+echo "--------------------------"
+
+echo "[!] Creating Directory : $HOME/.config/pwnedornot"
 mkdir -p $HOME/.config/pwnedornot
+
+echo "[!] Copying config.json..."
 cp config.json $HOME/.config/pwnedornot/config.json
+
+echo "[+] DONE"
